b68f571158d082d0860095b451240b3acafdd8d0
runs664 <harunsixsixfour@gmail.com>
2023-11-28 00:14:08 +0700

    first launch and commit yay

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const withNextra = require('nextra') (
+  {
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.js',
+    latex: true
+  }
+)
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withNextra(nextConfig)
---
+++ theme.config.js
@@ -0,0 +1,6 @@
+export default {
+    logo: <span>runs' personal page</span>,
+    project: {
+        link: 'https://github.com/runs664/quran_bot'
+    }
+}
