f16c3712bbb1e9f6404f55b554f8c30e78860449
runs664 <harunsixsixfour@gmail.com>
2023-11-30 22:22:02 +0700

    Add @vercel/analytics package

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import localFont from 'next/font/local';
 import { Cantarell } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/react";
 
 const cantarell = Cantarell({
     variable: '--cantarell',
@@ -11,5 +12,10 @@ const cantarell = Cantarell({
 const cmuRoman = localFont({src: '../public/font/cmunrm.ttf', variable: '--cmu-roman'});
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component className={cantarell.className} {...pageProps} />;
+    return (
+        <>
+        <Component className={cantarell.className} {...pageProps} />
+        <Analytics/>
+        </>
+    );
 }
